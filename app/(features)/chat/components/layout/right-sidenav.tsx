6f2dcb550312e365dfc8351372479562a5bae6c7
riccha <rimu.murakami.12@gmail.com>
2023-12-22 13:41:50 +0900

    概要欄途中

--- app/(features)/chat/components/layout/right-sidenav.tsx
+++ app/(features)/chat/components/layout/right-sidenav.tsx
@@ -1,17 +1,27 @@
 import { LuPencilLine, LuPlus } from "react-icons/lu";
 
-import { Avatar, Box, HStack, Text, VStack } from "@chakra-ui/react";
+import { useState } from "react";
+import { Avatar, Box, Button, HStack, Text, VStack } from "@chakra-ui/react";
 
 export function RightSideNav() {
+  const [overview, setOverview] = useState("概要を入力");
+  const [toggleOverview, setToggleOverview] = useState(false);
+
   return (
     <Box m={1} maxH={"calc(100dvh - 40px)"}>
       <Box h={"40dvh"}>
         <HStack p={1} h={"12"} justify={"space-between"} borderBottom={"1px solid"} borderColor={"gray.400"}>
           <Text>概要</Text>
-          <LuPencilLine size="24px" />
+          <Button color={"blackalpha.700"} p={0} onClick={() => setToggleOverview(!toggleOverview)}>
+            <LuPencilLine size="24px" />
+          </Button>
         </HStack>
         <Box pt={4} w={"100%"} h={"80%"} bgColor={"gray.100"}>
-          ここに概要
+          {toggleOverview ? (
+            `編集中`
+          ) : (
+            overview
+          )}
         </Box>
       </Box>
       <Box>
