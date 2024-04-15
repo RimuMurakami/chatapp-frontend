1c9c5c7549d6e7e2e74b6d8ff07e9758c573e2b6
Rimu Murakami <rimu.murakami.12@gmail.com>
2024-04-15 11:20:33 +0900

    errror fix

--- app/(features)/(auth)/components/oauth/_page.tsx
+++ app/(features)/(auth)/components/oauth/_page.tsx
@@ -27,7 +27,7 @@ export default function Login() {
           <Stack spacing={{ base: "2", md: "3" }} textAlign="center">
             <Heading size={{ base: "xs", md: "sm" }}>Log in to your account</Heading>
             <Text color="fg.muted">
-              Don't have an account? <Link href="#">Sign up</Link>
+              Don{"'"}t have an account? <Link href="#">Sign up</Link>
             </Text>
           </Stack>
         </Stack>
